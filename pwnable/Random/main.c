0a07baa848ff45d2a9b6bde8451e9c9b259c886f
Mark-74 <balduccimarco2007@gmail.com>
2024-06-06 14:47:34 +0200

    added pwnable repo

---
+++ pwnable/Random/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    
+    unsigned int random;
+    random = rand();
+    printf("%u", random);
+    
+    return 0;
+}
---
+++ pwnable/Random/sol.py
@@ -0,0 +1,1 @@
+print(0xdeadbeef ^ 1804289383)
